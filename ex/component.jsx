ee3d60924d8f45d2917ee20360c78ce07c9a6e87
nagpaulo <nagpaulo@gmail.com>
2018-06-26 21:13:47 -0300

    Segundo Component

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
-export default function(){
-    return <h1>Primeiro Componente</h1>
-}
+export default props => (
+    <h1>Primeiro Componente!</h1>
+);
+
+export const Segundo = props => (
+    <h1>Segundo Componente!</h1>
+);
