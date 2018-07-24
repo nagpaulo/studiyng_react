02285939946e74817d5a29cb301b5f87ff36970f
paulo.roberto <nagpaulo@gmail.com>
2018-07-24 15:46:48 -0300

    Reducer e Redux

--- ex.1/index.jsx
+++ ex.1/index.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import Field from './field';
-import fieldReducer from './fieldReducer';
 
 const reducers = combineReducers({
-    field: fieldReducer
+    field: () => ({ value: 'Opa'})
 });
 
 ReactDom.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <h1>Teste</h1>
     </Provider>
     , document.getElementById('app')
 );
---
+++ ex/fieldReducer.jsx
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {value: 'Opa'}
+export default function(state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case 'VALUE_CHANGE':
+            return { value: action.payload }
+        default: 
+            return state
+    }
+}
